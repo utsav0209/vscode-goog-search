75e42e6d979732db1e94832eb91717275bea74e7
utsav0209 <utsav99varia@gmail.com>
2021-07-30 15:59:46 +0530

    ENH: Added search suppport for editor and terminal

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,44 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
 // this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "goog-search" is now active!');
+  const URL = "https://www.google.com/search?q=";
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "goog-search.helloWorld",
+  // Update integrated terminal configuration to copy selected text
+  vscode.workspace
+    .getConfiguration()
+    .update(
+      "terminal.integrated.copyOnSelection",
+      true,
+      vscode.ConfigurationTarget.Global
+    );
+
+  let editorSearch = vscode.commands.registerCommand(
+    "goog-search.editor",
     () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from goog-search!");
+      var editor = vscode.window.activeTextEditor;
+
+      if (!editor) {
+        return; // No open text editor
+      }
+
+      const selection = editor.selection;
+      const text = editor.document.getText(selection);
+
+      vscode.env.openExternal(vscode.Uri.parse(`${URL}${text}`));
     }
   );
 
-  context.subscriptions.push(disposable);
+  let terminalSearch = vscode.commands.registerCommand(
+    "goog-search.terminal",
+    async () => {
+      const text = await vscode.env.clipboard.readText();
+
+      vscode.env.openExternal(vscode.Uri.parse(`${URL}${text}`));
+    }
+  );
+
+  context.subscriptions.push(editorSearch, terminalSearch);
 }
 
 // this method is called when your extension is deactivated
